0a68b3fb2d5b377e0fa1a7d6435c528b7d43226f
Felipe Brito <107517966+FelipeFJBrito@users.noreply.github.com>
2022-10-02 23:43:32 -0300

    Update Exercise5.py

--- Exercises/Lists/Exercise5.py
+++ Exercises/Lists/Exercise5.py
@@ -1,4 +1,5 @@
-
+#Faça um Programa que leia 20 números inteiros e armazene-os num vetor. 
+# Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três vetores.
 
 list_number = []
 list_even = []
@@ -8,7 +9,7 @@ print("Enter the numbers")
 for i in range(0,5):
     data = int(input())
     list_number.append(data)
-print(list_number)    
+print(f"The full list is {list_number}")    
 
 for i in list_number:
     if i % 2 == 0:
@@ -16,5 +17,5 @@ for i in list_number:
     else:
         list_odd.append(i)
 
-print(list_even)
-print(list_odd)
+print(f"The even list is {list_even}")  
+print(f"The odd list is {list_odd}")  
